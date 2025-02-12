fcc525047e84fb3439462cd3f0e0cc8823a9297f
张文杰 <826232419@qq.com>
2025-02-12 23:25:27 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <VirtualList></VirtualList>
+</template>
+
+<script setup>
+import VirtualList from './components/virtual-list.vue';
+
+
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  box-sizing: border-box;
+  text-align: center;
+  border: 1px solid black;
+}
+</style>
---
+++ src/components/virtual-list.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="container" >
+        <div class="list-scroll" @scroll="handeScrool">
+            <div class="list" :style="{transform:`translateY(${offset}px)`}">
+                <div class="list-item" v-for="item in randarList" :key="item">{{ 'item'+item }}</div>
+            </div>
+            
+        </div>
+    </div>
+</template>
+<script setup>
+import { computed, ref } from 'vue';
+
+let data = Array.from({length:100},(_,i)=>i)
+
+const maxCount = 9
+// const tottalheight = 50*maxCount
+const startIndex = ref(0)
+const endIndex = ref(0)
+const scrollTop = ref(0)
+
+
+const offset = computed(()=>{
+    return startIndex.value * 50
+})
+
+const randarList = computed(()=>{
+   
+    return data.slice(startIndex.value,endIndex.value==0?maxCount:endIndex.value)
+})
+
+
+
+
+
+const handeScrool = (e)=>{
+   // e.target.scrollHeight 滚动条高度
+   // e.target.scrollTop 滚动条距离其顶部的距离（以px为单位）
+   scrollTop.value = e.target.scrollTop
+   startIndex.value = Math.floor(scrollTop.value/50)
+   endIndex.value = startIndex.value+maxCount
+    
+}
+
+</script>
+<style>
+    *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    }
+    .container{
+        width: 200px;
+        height: 400px;
+        border: 1px solid black;
+        margin: 0 auto;
+        .list-scroll{
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            .list{
+            width: 100%;
+            height: 100%;
+            .list-item{
+                width: 100%;
+                height: 50px; 
+                line-height: 50px;
+                background-color: antiquewhite;
+                border: 1px solid black;
+            }
+        }
+        }
+       
+    }
+</style>
